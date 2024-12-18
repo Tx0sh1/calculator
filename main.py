798a74894793121cfee3718b5481aeaf7f0dee04
Tx0sh1 <clydemojura@protonmail.com>
2024-12-18 23:22:17 +0200

    - add loop - fix division error message - refactor code

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 print("Hey, Welcome to my basic calculator")
 
-
-operator = input("enter 'add' for addition, 'sub' for subtraction, 'div' for division and 'times' for multiplication: \n")
-
-
-
-a = float(input("Enter your first number \n"))
-b = float(input("enter your second number \n"))
-
-
 def add(a, b):
     return a + b
 def sub(a, b):
@@ -17,16 +8,32 @@ def div(a, b):
     if b != 0:
         return a / b
     else:
-        print("cannot divide by 0")
+        return "undefined"
 def times(a, b):
     return a * b
 
 
-if operator == "add":
-    print(add(a, b))
-elif operator == "div":
-    print(div(a, b))
-elif operator == "sub":
-    print(sub(a, b))
-elif operator == "times":
-    print(times(a, b))
+
+while True:
+    
+    operator = input("enter 'add' for addition, 'sub' for subtraction, 'div' for division and 'times' for multiplication: \n").lower()
+    
+    if operator == "exit":
+        print("Goodbye!")
+        break
+    try:
+        a = float(input("Enter your first number \n"))
+        b = float(input("enter your second number \n"))
+    except ValueError:
+        print("Invalid input!, please enter a number")
+    
+    if operator == "add":
+        print(add(a, b))
+    elif operator == "div":
+        print(div(a, b))
+    elif operator == "sub":
+        print(sub(a, b))
+    elif operator == "times":
+        print(times(a, b))
+    else:
+        print("invalid operator! try again")
